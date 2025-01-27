e890fba61ab12cd181a156e1f34a0528104f92db
DavidIsrael <davidisrael.rj@gmail.com>
2025-01-27 06:43:59 -0300

    Componete Multi

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Text, View } from "react-native";
 import Primeiro from "./components/Primeiro";
+import Comp, { Comp1, Comp2 } from "./components/Multi";
 
 function App() {
     return <View>
+        <Comp/>
+        <Comp1/>
+        <Comp2/>
         <Primeiro/>
     </View>
 }
