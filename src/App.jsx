b5faa525cbf0b9453b211af15ff691bac9a89ddb
DavidIsrael <davidisrael.rj@gmail.com>
2025-01-27 22:09:01 -0300

    Componente com Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Primeiro from "./components/Primeiro";
-import Comp, { Comp1, Comp2 } from "./components/Multi";
+import { StyleSheet, View } from "react-native";
+// import Primeiro from "./components/Primeiro";
+// import Comp, { Comp1, Comp2 } from "./components/Multi";
+import MinMax from "./components/MinMax";
 
 function App() {
     return <View style={style.App}>
-        <Comp/>
+        <MinMax min="3" max="20"/>
+        <MinMax min="50" max="249"/>
+        {/* <Comp/>
         <Comp1/>
         <Comp2/>
-        <Primeiro/>
+        <Primeiro/> */}
     </View>
 }
 
@@ -17,7 +20,8 @@ const style = StyleSheet.create({
         backgroundColor:"#fff",
         flex: 1,
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
+        padding: 20
     }
 })
 
