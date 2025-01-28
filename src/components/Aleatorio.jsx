97cb3b43dca89d0220526c3868f1cfaec0338aa3
DavidIsrael <davidisrael.rj@gmail.com>
2025-01-28 06:33:19 -0300

    Número Aleatório - professor

--- src/components/Aleatorio.jsx
+++ src/components/Aleatorio.jsx
@@ -3,11 +3,10 @@ import { Text } from "react-native";
 import estilo from "./estilo";
 
 export default (props) =>{
-    const min = Math.ceil(props.min)
-    const max = Math.floor(props.max)
-    const aleatorio = Math.floor(Math.random() * (max -min + 1)) + min
-    
+
+    const delta = props.max - props.min +1
+    const aleatorio2 = parseInt(Math.random() * delta) + props.min
     return(
-        <Text style={estilo.fontG}>Número Aleatório entre {props.min} e {props.max} é {aleatorio}</Text>
+        <Text style={estilo.fontG}>Número Aleatório entre {props.min} e {props.max} é {aleatorio2}</Text>
     )
 }
