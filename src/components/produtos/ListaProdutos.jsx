b291cf36682057333e99545f087e88bbf1aafb86
DavidIsrael <davidisrael.rj@gmail.com>
2025-01-31 20:04:59 -0300

    Função ObterLista

--- src/components/produtos/ListaProdutos.jsx
+++ src/components/produtos/ListaProdutos.jsx
@@ -4,14 +4,17 @@ import Estilo from '../estilo'
 import produtos from './produtos';
 
 export default props => {
+    function obterLista(){
+        return produtos.map(p=>{
+            return <Text key={p.id} style={Estilo.fontM}>{p.id}) {p.nome} por R$ {p.preco}</Text>
+        })
+    }
     return (
         <>
             <Text style={Estilo.fontG}>
                 Lista de produtos
             </Text>
-            {produtos.map(p =>{
-                return <Text style={Estilo.fontM} key={p.id}>{p.id} {p.nome} por R$ {p.preco}</Text>
-            })}
+            {obterLista()}
         </>
     )
 }
