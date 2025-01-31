6e09e2045aee689beca7f7b8a3b8818db069b13e
DavidIsrael <davidisrael.rj@gmail.com>
2025-01-31 06:56:34 -0300

    Constante recebendo props ou um valor padrão

--- src/components/UsuarioLogado.jsx
+++ src/components/UsuarioLogado.jsx
@@ -3,7 +3,8 @@ import { Text } from 'react-native'
 import Estilo from './estilo'
 import If from './if';
 
-export default ({ usuario = {} }) => {
+export default props => {
+    const usuario = props.usuario || {}
     return (
         <>
         <If teste={usuario && usuario.nome && usuario.email}>
