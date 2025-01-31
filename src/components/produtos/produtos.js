48543f9d23a5a0afbbab357ae115be24a24bbb7e
DavidIsrael <davidisrael.rj@gmail.com>
2025-01-31 19:40:10 -0300

    Produtos

---
+++ src/components/produtos/produtos.js
@@ -0,0 +1,12 @@
+export default [
+    { id: 1, nome: 'Lapiseira', preco: 9.99 },
+    { id: 2, nome: 'Caderno grande', preco: 19.99 },
+    { id: 3, nome: 'Cardeneta', preco: 4.99 },
+    { id: 4, nome: 'Mochila', preco: 49.99 },
+    { id: 5, nome: 'Caneca', preco: 9.99 },
+    { id: 6, nome: 'Caneta', preco: 7.99 },
+    { id: 7, nome: 'Borracha', preco: 3.99 },
+    { id: 8, nome: 'Apontador', preco: 2.99 },
+    { id: 9, nome: 'Lapis de Cor', preco: 14.99 },
+    { id: 10, nome: 'Canetinha', preco: 29.99 },
+]
